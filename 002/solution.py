0db3edaaaf6a87828044a5ea4d800c02675f4807
Nap Joseph Calub <njncalub@gmail.com>
2019-12-16 11:29:19 +0800

    [002] Added description for problem 002.

---
+++ 002/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def solve(N: List[int]) -> List[int]:
+    """Returns a list of products, excluding the element at position i.
+
+    Args:
+        N: a list of integers
+
+    Returns:
+        A list where each element at index i is the product of all the numbers
+        in the original list, except the one at i.
+    """
+
+    return []
---
+++ 002/test_solution.py
@@ -0,0 +1,25 @@
+import pytest
+
+from dataclasses import dataclass
+from typing import List
+
+from solution import solve
+
+
+@dataclass
+class Case:
+    arg: List[int]
+    answer: List[int]
+
+
+def test_solve():
+    cases: List[Case] = [
+        Case([1, 2, 3, 4, 5], [120, 60, 40, 30, 24]),
+        Case([3, 2, 1], [2, 3, 6]),
+        Case([0, 3, 5, 7], [105, 0, 0, 0]),
+        Case([-1, 3, -2, 1], [-6, 2, -3, 6]),
+        Case([], []),
+    ]
+
+    for c in cases:
+        assert solve(c.arg) == c.answer
