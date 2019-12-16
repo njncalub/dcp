99993963461605a70e4159b2590fdfa42f1d4ea1
Nap Joseph Calub <njncalub@gmail.com>
2019-12-16 16:54:56 +0800

    [002] Refactored brute-force solution. Found a semi-dynamic programming approach.

--- 002/solution.py
+++ 002/solution.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 def solve(N: List[int]) -> List[int]:
     """Returns a list of products, excluding the element at position i.
 
@@ -11,11 +12,17 @@ def solve(N: List[int]) -> List[int]:
         in the original list, except the one at i.
     """
 
-    # Brute force approach using O(n(n-1)):
-    # - Loop through all numbers n of N
-    # - Generate a new array without n and multiply all the items in that array
-    # - Append the product to the solution set
-    # - If we passed a zero, it means the rest would have a product of 0
+    return two_loops(N)
+
+
+def brute_force(N: List[int]) -> List[int]:
+    """Brute force approach using O(n(n-1)):
+
+    - Loop through all numbers n of N
+    - Generate a new array without n and multiply all the items in that array
+    - Append the product to the solution set
+    - If we passed a zero, it means the rest would have a product of 0
+    """
 
     answer = []
     for index in range(len(N)):
@@ -36,3 +43,44 @@ def solve(N: List[int]) -> List[int]:
             break
 
     return answer
+
+
+def two_loops(N: List[int]) -> List[int]:
+    """Semi-dynamic programming approach using O(2n):
+
+    - Calculate the product of all items before item i
+    - Calculate the product of all items after item i
+    - For each item i, multiply the products for before and after i
+
+    N = [3, 7, 1, 4, 8, 9]
+    i = [0, 1, 2, 3, 4, 5]
+
+    L[0] = 1 = 1
+    L[1] = (1) * 3 = 3
+    L[2] = (3) * 7 = 21
+    L[3] = (21) * 1 = 21
+    L[4] = (21) * 4 = 84
+    L[5] = (84) * 8 = 672
+
+    R[0] = (288) * 7 = 2016
+    R[1] = (288) * 1 = 288
+    R[2] = (72) * 4 = 288
+    R[3] = (9) * 8 = 72
+    R[4] = (1) * 9 = 9
+    R[5] = 1 = 1
+
+    A = [L[0]*R[0], L[1]*R[1], L[2]*R[2], L[3]*R[3], L[4]*R[4], L[5]*R[5]]
+    A = [2016, 864, 6048, 1512, 756, 672]
+    i = [   0,   1,    2,    3,   4,   5]
+    """
+
+    items_len = len(N)
+    of_left = [1 for _ in range(items_len)]
+    of_right = [1 for _ in range(items_len)]
+    for i in range(items_len):
+        j = (items_len - 1) - i  # Invert i, start counting from len(N) to 0.
+        if i != 0:
+            of_left[i] = N[i-1] * of_left[i-1]
+            of_right[j] = N[j+1] * of_right[j+1]
+
+    return list(map(lambda p: p[0] * p[1], zip(of_left, of_right)))
